d7b928a34615bb9bbf3608daabdd8416ad4bcf14
Afrimann <omotoshopeter737@gmail.com>
2024-07-10 23:02:52 +0100

    files updated

--- src/ShowResult.jsx
+++ src/ShowResult.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ShowResult = ({cgpa}) => {
   return (
     <div>
-      <p>Your CGPA is : {cgpa}</p>
+      <p style={{fontSize : '20px' ,fontWeight: 'bold'}}>Your CGPA is : {cgpa}</p>
     </div>
   )
 }
